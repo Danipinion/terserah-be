83a5ec336c0bb022c94bbd4f257d6527931c9f89
Mohammad Dani Taufiqurrohman <mlymikro@gmail.com>
2024-05-27 16:37:32 +0700

    ⚙️: fix cors policy

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
+app.use(
+  cors({
+    origin: "https://terserah-one.vercel.app",
+  })
+);
 app.use(express.json());
 app.use(ReviewsRouter);
 
 app.listen(process.env.APP_PORT, () => {
-	console.log(`Listening on port ${process.env.APP_PORT}`);
+  console.log(`Listening on port ${process.env.APP_PORT}`);
 });
